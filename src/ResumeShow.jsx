246291728d70123517a5f27ed9347c131f49c5cc
Adam Folta <afolta@thecitybase.com>
2022-12-27 09:59:39 -0500

    remove default values

--- src/ResumeShow.jsx
+++ src/ResumeShow.jsx
@@ -8,35 +8,35 @@ export function ResumeShow(props) {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <div>Student Info:</div>
+        <h1>Student Info:</h1>
         <div>
-          First Name: <input defaultValue="props.resume.first_name" name="first_name" type="text" />
+          First Name: <input name="first_name" type="text" />
         </div>
         <div>
-          Last Name: <input defaultValue="props.resume.last_name" name="last_name" type="text" />
+          Last Name: <input name="last_name" type="text" />
         </div>
         <div>
-          Email: <input defaultValue="props.resume.email" name="email" type="text" />
+          Email: <input name="email" type="text" />
         </div>
         <div>
-          Phone Number: <input defaultValue="props.resume.phone_number" name="phone_number" type="number" />
+          Phone Number: <input name="phone_number" type="number" />
         </div>
-        <div>Skills:</div>
-        <div>Experience:</div>
+        <h1>Skills:</h1>
+        <h1>Experience:</h1>
         <div>
-          Company Name: <input defaultValue="props.resume.company_name" name="company_name" type="text" />
-        </div>
-        <div>
-          Job Title: <input defaultValue="props.resume.job_title" name="job_title" type="text" />
+          Company Name: <input name="company_name" type="text" />
         </div>
         <div>
-          Start Date: <input defaultValue="props.resume.start_date" name="start_date" type="date" />
+          Job Title: <input name="job_title" type="text" />
         </div>
         <div>
-          End Date: <input defaultValue="props.resume.end_date" name="start_date" type="date" />
+          Start Date: <input name="start_date" type="date" />
         </div>
         <div>
-          Details: <input defaultValue="props.resume.details" name="details" type="paragraph" />
+          End Date: <input name="start_date" type="date" />
+        </div>
+        <div>
+          Details: <input name="details" type="paragraph" />
         </div>
         <button type="submit">Update resume</button>
       </form>
