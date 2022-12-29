af273f0d974878900a46cc910f32a7111e6998b7
Adam Folta <afolta@thecitybase.com>
2022-12-28 20:26:48 -0500

    new

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,44 +6,50 @@ export function Home() {
   const [isResumesVisible, setIsResumesShowVisible] = useState(false);
   const [currentResume, setCurrentResume] = useState({});
   const [resumes, setResumes] = useState([]);
+  const [id, setId] = useState(setId === 1);
 
-  // const resumes = [
-  //   {
-  //     id: 1,
-  //     student: {
-  //       photo: "https://via.placeholder.com/150",
-  //       first_name: "First",
-  //       last_name: "Last Name",
-  //       short_bio: "I'm a rockstar",
-  //       online_resume_url: "https://via.placeholder.com/150",
-  //       experience: { start_date: "02 / 01 / 2019", end_date: "03 / 02 / 2022", job_title: "Analyst" },
-  //     },
-  //   },
-  //   {
-  //     id: 2,
-  //     photo: "https://via.placeholder.com/300",
-  //     first_name: "Second",
-  //     last_name: "Last Name",
-  //     online_resume_url: "https://via.placeholder.com/150",
-  //     experience: { start_date: "02 / 01 / 2019", end_date: "03 / 02 / 2022", job_title: "Analyst" },
-  //   },
-  // ];
+  // const handleUpdateResume = (id, params, successCallback) => {
+  //   console.log("handleUpdateResume", params);
 
-  const handleUpdateResume = (params, successCallback) => {
-    console.log("handleUpdateResume", params);
+  //   axios.patch(`http://localhost:3000/students/+ id + “.json”.json`, params).then((response) => {
+  //     setResumes(
+  //       resumes.map((resume) => {
+  //         if (resume.id === response.data.id) {
+  //           return response.data;
+  //         } else {
+  //           return resume;
+  //         }
+  //       })
+  //     );
+  //     successCallback();
+  //     handleClose();
+  //   });
+  // };
 
-    axios.get(`http://localhost:3000/students/1.json`, params).then((response) => {
-      setResumes(
-        resumes.map((resume) => {
-          if (1 === response.data.id) {
-            return response.data;
-          } else {
-            return resume;
-          }
-        })
-      );
-      successCallback();
-      handleClose();
+  const handleUpdateResume = (id, params) => {
+    axios
+      .patch("http://localhost:3000/students/" + id + ".json", params)
+      .then((response) => {
+        console.log(response.data);
+        setResumes(
+          resumes.map((resume) => {
+            if (1 === id) {
+              return response.data;
+            } else {
+              return resume;
+            }
+          })
+        );
+      })
+      .catch((error) => {
+        console.log(error.response.data.errors);
+      });
+  };
+
+  const handleIndexResumes = () => {
+    axios.get("http://localhost:3000/students.json").then((response) => {
+      console.log(response.data);
+      setResumes(response.data);
     });
   };
 
